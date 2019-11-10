bc754b8f939894d70904ba628d9d052dc697411c
Shankar Raghupathi <shnkr@live.in>
2019-11-11 01:51:28 +0530

    Unity 2019

---
+++ Always.Kanye.Code/Assets/Scripts/Kanye.Rest.data/KanyeRestAnswers.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class KanyeRestAnswers
+{
+    public string _Answer;
+
+    public Vector2 _DurationRange;
+}
